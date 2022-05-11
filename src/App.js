d55f43fc755fcbae08ec11f8e05c56618c75e231
kevin <kevinkerjaan@gmail.com>
2022-05-11 17:47:39 +0700

    04 - React State & Working with Events

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
-
+import NewExpense from './components/NewExpense/NewExpense';
 const App = () => {
   const expenses = [{
       id: 'e1',
@@ -27,9 +27,14 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log('in app.js');
+    console.log(expense);
+  }
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
